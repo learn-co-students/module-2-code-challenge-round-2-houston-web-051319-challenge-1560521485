d909bdc6769126616eda288566358b117e1e4b71
Maruf <marufbinawal@gmail.com>
2019-06-14 10:51:24 -0500

    fingers crossed

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -1,6 +1,50 @@
 class GuestsController < ApplicationController
+    def index 
+        @guests= Guest.all
+        
+    end
 
-  def index
-    @guests = Guest.all
-  end
+    def show
+        @guest = Guest.find(params[:id])
+    
+    end
+
+    def new
+        @guest = Guest.new
+        @episodes = Episode.all
+    
+    end
+
+    def create
+        @guest = Guest.new(guest_params)
+
+        if(@guest.save)
+            redirect_to @guest
+        else 
+                render 'new'
+        end
+    end
+
+    private def guest_params
+        params.require(:guest).permit(:name, :occupation, :episode_id, :episode_rating)
+    
+    end
+
+    def edit
+        @guest = Guest.find(params[:id])
+    
+    end
+
+    def update
+        @employee = Employee.find(params[:id])
+
+        if(@employee.update(employee_params))
+            redirect_to @employee
+        else 
+                render 'edit'
+        end
+    
+    end
+
 end
+
