7de792272ba1a41d6cdb02b417b2f47d57250404
Hans Krohn <hanskrohn70@gmail.com>
2019-06-14 10:43:02 -0500

    deleted everything one mayor glitch found it better to restart didnt look at time

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -3,4 +3,31 @@ class GuestsController < ApplicationController
   def index
     @guests = Guest.all
   end
+  def show
+    @guest = Guest.find(params[:id])
+  end
+  def edit
+
+  end
+  def update
+
+  end
+  def new
+
+  end
+  def create
+
+  end
+  def delete
+
+  end
+  def guest_params
+    params.permit(
+      guest: [
+        :name,
+        :occupation,
+        :appearances
+      ]
+    )
+  end
 end
